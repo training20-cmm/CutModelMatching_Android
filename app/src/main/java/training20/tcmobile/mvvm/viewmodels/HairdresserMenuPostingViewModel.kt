a5ed0430a859abd23c68a1acc3f6cb447502683f
r.kamata <r.kamata@tcmobile.jp>
2020-06-23 17:12:48 +0900

    ViewModel_start処理、チェックボックスの情報を受け取る下準備,Fragmentに伝達追加

--- app/src/main/java/training20/tcmobile/mvvm/viewmodels/HairdresserMenuPostingViewModel.kt
+++ app/src/main/java/training20/tcmobile/mvvm/viewmodels/HairdresserMenuPostingViewModel.kt
@@ -14,20 +14,27 @@ class response_sample(
     val name:String
 )
 
+class menuPostingpram(
+    val title:String
+)
+
 class HairdresserMenuPostingViewModel(
     eventDispatcher: EventDispatcher<HairdresserMenuPostingActions>
 ): BackableViewModel<HairdresserMenuPostingActions>(eventDispatcher)
 {
     fun start() {
-        //　リポジトリからデータ撮ってくる処理
-        val id = ""
-        val name = ""
+        //　リポジトリからデータ撮ってくる処理、が今回は初期値で持っているので省略。
+//        val id = ""
+//        val name = ""
         val res = arrayOf(response_sample(1, "施術1"),
             response_sample(2, "施術2"), response_sample(3, "施術3"),
             response_sample(4, "施術4"), response_sample(5, "施術5"))
+
+        onHairdresserOrdersSuccess(res)
     }
 
-    fun dataCatch(response:Array<response_sample>) {
+
+    fun onHairdresserOrdersSuccess(response:Array<response_sample>) {
         // 処理省略
         eventDispatcher.dispatchEvent { onTreatmentChanged() }
     }
