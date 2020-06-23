71cc389a5634962f16f6535df1c61ebd895d75f2
r.kamata <r.kamata@tcmobile.jp>
2020-06-23 17:13:57 +0900

    Fragment_受け取ったあとの処理追加

--- app/src/main/java/training20/tcmobile/fragments/HairdresserMenuPostingFragment.kt
+++ app/src/main/java/training20/tcmobile/fragments/HairdresserMenuPostingFragment.kt
@@ -6,11 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.flexbox.*
+import kotlinx.android.synthetic.main.fragment_hairdresser_menu_posting.view.*
 import org.koin.android.ext.android.inject
 import training20.tcmobile.R
 import training20.tcmobile.databinding.FragmentHairdresserMenuPostingBinding
 import training20.tcmobile.mvvm.actions.HairdresserMenuPostingActions
 import training20.tcmobile.mvvm.viewmodels.HairdresserMenuPostingViewModel
+import training20.tcmobile.ui.recyclerview.adapters.HairdresserMenuPostingRecyclerViewAdpter
 
 
 //　viewを操作するところ
@@ -22,16 +25,30 @@ class HairdresserMenuPostingFragment :
 {
     override val viewModel: HairdresserMenuPostingViewModel by inject()
 
+    private val airdresserMenuPostingRecyclerViewAdapter = HairdresserMenuPostingRecyclerViewAdpter(viewModel)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // 最初に画面表示するためのものを書いていこう
+        // チェックボックスを表示させるための処理
+        val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
+        val menupostingRecycler = view.orderRecycle_view
+
         return inflater.inflate(R.layout.fragment_hairdresser_menu_posting, container, false)
     }
 
+    // flexboxLayoutのセットアップ
+    fun layoutSetup() {
+        val flexboxLayoutManager = FlexboxLayoutManager(activity)
+        flexboxLayoutManager.flexDirection = FlexDirection.COLUMN
+        flexboxLayoutManager.flexWrap = FlexWrap.WRAP
+        flexboxLayoutManager.justifyContent = JustifyContent.SPACE_AROUND
+        flexboxLayoutManager.alignItems = AlignItems.STRETCH
+    }
 
-    //　写真選択&表示するよ
+    //　写真選択&表示するメソッドとりあえず置いた
     private fun selectPhoto() {
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
@@ -71,6 +88,7 @@ class HairdresserMenuPostingFragment :
 
     override fun setupViewModel(viewModel: HairdresserMenuPostingViewModel) {
         viewModel.eventDispatcher.bind(viewLifecycleOwner, this)
+        viewModel.start()
     }
 
     override fun setupDataBinding(
@@ -81,7 +99,8 @@ class HairdresserMenuPostingFragment :
     }
 
     //　ViewModelから情報を取得したお知らせをもらう
+    // もらったら画面を再構成する(チェックボックス表示のため)
     override fun onTreatmentChanged() {
-
+        airdresserMenuPostingRecyclerViewAdapter.notifyDataSetChanged()
     }
 }
