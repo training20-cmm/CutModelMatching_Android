c84728540616da4db7ae4df97b8c39c9f75cc1ad
so.yoshida <s.yoshida@tcmobile.jp>
2020-06-25 11:23:34 +0900

    up

--- app/src/main/java/training20/tcmobile/fragments/HairdresserSalonRegistrationFragment.kt
+++ app/src/main/java/training20/tcmobile/fragments/HairdresserSalonRegistrationFragment.kt
@@ -5,15 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_model_registration_form.*
+import kotlinx.android.synthetic.main.fragment_hairdresser_salon_registration.view.*
+import org.koin.android.ext.android.inject
 import training20.tcmobile.R
+import training20.tcmobile.databinding.FragmentHairdresserSalonRegistrationBinding
+import training20.tcmobile.mvvm.MvvmFragment
+import training20.tcmobile.mvvm.actions.HairdresserSalonRegistrationActions
+import training20.tcmobile.mvvm.viewmodels.HairdresserRegistrationFormViewModel
+import training20.tcmobile.mvvm.viewmodels.HairdresserSalonRegistrationViewModel
 
-class HairdresserSalonRegistrationFragment : Fragment() {
+class HairdresserSalonRegistrationFragment:
+    BackableFragment<HairdresserSalonRegistrationActions, FragmentHairdresserSalonRegistrationBinding, HairdresserSalonRegistrationViewModel>(),
+    HairdresserSalonRegistrationActions
+{
+
+    override val viewModel: HairdresserSalonRegistrationViewModel by inject()
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setupRegistrationButton()
+//    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hairdresser_salon_registration, container, false)
+        val view = super.onCreateView(inflater, container, savedInstanceState) ?: return null
+        setupRegistrationButton(view)
+        return view
+    }
+
+    override fun createDataBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHairdresserSalonRegistrationBinding = FragmentHairdresserSalonRegistrationBinding.inflate(inflater, container, false)
+
+    override fun setupViewModel(viewModel: HairdresserSalonRegistrationViewModel) {
+        viewModel.eventDispatcher.bind(viewLifecycleOwner, this)
+    }
+
+    override fun setupDataBinding(
+        dataBinding: FragmentHairdresserSalonRegistrationBinding,
+        savedInstanceState: Bundle?
+    ) {
+        dataBinding.viewModel = viewModel
+    }
+
+    private fun setupRegistrationButton(view: View) {
+        view.registrationButton.setOnClickListener(this::onRegistrationButtonClicked)
+    }
+
+    private fun onRegistrationButtonClicked(v: View) {
+        view?.registrationButton?.visibility = View.GONE
+        view?.registrationSpinner?.visibility = View.VISIBLE
+        viewModel.registerHairdresser()
     }
 }
--- app/src/main/java/training20/tcmobile/mvvm/viewmodels/HairdresserSalonRegistrationViewModel.kt
+++ app/src/main/java/training20/tcmobile/mvvm/viewmodels/HairdresserSalonRegistrationViewModel.kt
@@ -1,9 +1,33 @@
 package training20.tcmobile.mvvm.viewmodels
 
+import androidx.databinding.Observable
+import androidx.databinding.ObservableInt
 import training20.tcmobile.mvvm.actions.HairdresserSalonRegistrationActions
 import training20.tcmobile.mvvm.event.EventDispatcher
+import training20.tcmobile.net.http.responses.HairdresserRegistrationResponse
 
 class HairdresserSalonRegistrationViewModel(
     eventDispatcher: EventDispatcher<HairdresserSalonRegistrationActions>
 ): BackableViewModel<HairdresserSalonRegistrationActions>(eventDispatcher) {
+    var salonname = ""
+    var salonmemo = ""
+    var postalcode = ""
+    var prefecturePosition = 0
+    var address = ""
+    var residence = ""
+    var seatsnumber = ""
+    //var gender: ObservableInt? = null
+    var cash = false
+    var credit = false
+    var starttime = ""
+    var endtime = ""
+    var starttime2 = ""
+    var endtime2 = ""
+    var weekPosition = 0
+
+
+    fun registerHairdresser() {
+            print(salonname)
+    }
+
 }
